86176222011cf31780c1777ebf8c6103843cae0e
Roberto-Livi <robertomlivi@outlook.com>
2021-04-15 16:49:30 -0400

    setting up Enzyme

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom'
 import App from '../App'
 
 it('shows a comment box', () => {
-    
+    const div = document.create(div);
+
+    ReactDOM.render(<App />, div);
+
+    ReactDOM.unmountComponentAtNode(div);
 })
