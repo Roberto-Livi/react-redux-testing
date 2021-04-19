c95686fdec7863715fc214a1829ce3f4ffbc7d96
Roberto-Livi <robertomlivi@outlook.com>
2021-04-18 20:35:59 -0500

    using beforeEach and afterEach for CommentBox testing

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import CommentBox from 'components/CommentBox'
+import { mount } from 'enzyme'
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = mount(<CommentBox />);
+})
+
+afterEach(() => {
+    // unmounts the component that was created. Won't have to worry about other components that are created in other tests.
+    wrapped.unmount();
+})
 
 it('has a textarea and a button', () => {
-    
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
 })
